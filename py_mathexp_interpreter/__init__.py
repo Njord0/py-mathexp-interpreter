878790b14ddefb8537fa26153e09a738ce97b96c
Njord0 <52111877+Njord0@users.noreply.github.com>
2020-12-17 17:02:45 +0100

    Add utils.py

--- py_mathexp_interpreter/__init__.py
+++ py_mathexp_interpreter/__init__.py
@@ -1,3 +1,4 @@
 from .lexer import Lexer
 from .parser import Parser
-from .interpreter import Interpreter, funcs
+from .interpreter import Interpreter
+from .utils import add_function, add_constant
